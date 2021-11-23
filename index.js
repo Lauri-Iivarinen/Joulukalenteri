bc766c072fedfdac801cf4730ade91e86239aff6
Lauri Iivarinen <lauri@netti123.local>
2021-11-23 15:05:38 +0200

    for loopit 1000 rivin tilalle

--- index.js
+++ index.js
@@ -1,67 +1,39 @@
+var text = "*"
+var barText = ""
 
-var interval =3;
 
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES</h1>"
-},700)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES.</h1>"
-},1400)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES..</h1>"
-},2100)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES...</h1>"
-},2800)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES</h1>"
-},3500)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES.</h1>"
-},4200)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES..</h1>"
-},4900)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES...</h1>"
-},5600)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES</h1>"
-},6300)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES.</h1>"
-},7000)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES..</h1>"
-},7700)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>ACCESSING FILES...</h1>"
-},8400)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS</h1>"
-},9100)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS.</h1>"
-},9800)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS..</h1>"
-},10500)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS...</h1>"
-},11200)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS</h1>"
-},11900)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS.</h1>"
-},12600)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS..</h1>"
-},13300)
-setTimeout(function(){
-    document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS...</h1>"
-},14000)
-setTimeout(function(){
-    window.location.href = "kalenteri/kalenteri.html"
-},14700)
+var timer = 600;
+var i = 0;
 
+setInterval(function(){
+        i++
+        if(i>=10){
+            downloading();
+        }else if(i%4===1){
+            document.getElementById("start").innerHTML = "<h1>ACCESSING FILES.</h1>"
+        }else if(i%4===2){
+            document.getElementById("start").innerHTML = "<h1>ACCESSING FILES..</h1>"
+        }else if(i%4===3){
+            document.getElementById("start").innerHTML = "<h1>ACCESSING FILES...</h1>"
+        }else{
+            document.getElementById("start").innerHTML = "<h1>ACCESSING FILES</h1>"
+        }
+
+},timer)
+
+function downloading(){
+    var counter=0;
+    setInterval(function(){
+        counter++;
+        if(counter>=10){
+            window.location.href = "kalenteri/kalenteri.html"
+        }else{
+        document.getElementById("barStart").innerHTML = "["
+        document.getElementById("barEnd").innerHTML = "]"
+        barText += text
+        document.getElementById("start").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS</h1>"
+        document.getElementById("bar").innerHTML = barText
+        }
+
+},timer)
+}
