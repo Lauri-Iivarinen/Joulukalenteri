826b5599bee35928878a79c6fdba54c878afb2e0
Lauri Iivarinen <lauri@netti123.local>
2021-11-23 11:33:29 +0200

    toimiva kalenteripohja

--- kalenteri/kalenteri.js
+++ kalenteri/kalenteri.js
@@ -3,12 +3,12 @@ for (i=1; i<=24; i++){
     if ((i-1) %6  == 0){
         taulu += "<br>"
     }
-    taulu += "<td id='day"+i+"'><button onclick='funktio"+i+"();'>"+i+"</button></td>"
+    taulu += "<td><button class ='taulukko' id='day"+i+"' onclick='funktio();'>"+i+"</button></td>"
 }
 
 document.getElementById("table").innerHTML = taulu;
 
-function funktio1(){
+function funktio(){
     console.log("DAY 1")
-    document.getElementById("day1").innerHTML = "<tdPÄIVÄ 1</td>"
+    document.getElementById("day1").style.backgroundColor = "black";
 }
