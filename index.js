a6d579f8abc49cd0c9c73622d06ad139d8030b19
Lauri Iivarinen <lauri@netti123.local>
2021-11-23 10:43:45 +0200

    alustus kaks

---
+++ index.js
@@ -0,0 +1,63 @@
+
+var interval =3;
+
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES</h1>"
+},700)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES.</h1>"
+},1400)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES..</h1>"
+},2100)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES...</h1>"
+},2800)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES</h1>"
+},3500)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES.</h1>"
+},4200)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES..</h1>"
+},4900)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES...</h1>"
+},5600)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES</h1>"
+},6300)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES.</h1>"
+},7000)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES..</h1>"
+},7700)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>ACCESSING FILES...</h1>"
+},8400)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS</h1>"
+},9100)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS.</h1>"
+},9800)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS..</h1>"
+},10500)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS...</h1>"
+},11200)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS</h1>"
+},11900)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS.</h1>"
+},12600)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS..</h1>"
+},13300)
+setTimeout(function(){
+    document.getElementById("lol").innerHTML = "<h1>OPENING /DOCUMENTS/TOP _SECRET/XMAS...</h1>"
+},14000)
