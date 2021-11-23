77b37e615c1e584571d40fb39274dd0874a05a37
Lauri Iivarinen <lauri@netti123.local>
2021-11-23 11:46:51 +0200

    päivämäärätarkistus

--- kalenteri/kalenteri.js
+++ kalenteri/kalenteri.js
@@ -3,12 +3,32 @@ for (i=1; i<=24; i++){
     if ((i-1) %6  == 0){
         taulu += "<br>"
     }
-    taulu += "<td><button class ='taulukko' id='day"+i+"' onclick='funktio();'>"+i+"</button></td>"
+    taulu += "<td><button class ='taulukko' id='day"+i+"' onclick='funktio("+i+");'>"+i+"</button></td>"
 }
 
 document.getElementById("table").innerHTML = taulu;
 
-function funktio(){
-    console.log("DAY 1")
-    document.getElementById("day1").style.backgroundColor = "black";
+function funktio(day){
+    if(checkDay(day)){
+        console.log("DAY 1")
+        document.getElementById("day1").style.backgroundColor = "black";
+    }else{
+        console.log("wrong day");
+    }
+    
+}
+
+
+//palauttaa true jos klikattu luukku vastaa kyseistä päivämäärää
+function checkDay(day) {
+    var date = new Date();
+    var paivamaara = date.getDate()
+    var month = date.getMonth();
+    //month pitää vaihtaa joulukuussa "11"
+    if (paivamaara >= day && month == 10){
+        return true;
+    }
+    /*Seuraava return true pitää poistaa valmiista ohjelmasta,
+    se on olemassa vain testausta varten*/
+    return true;
 }
