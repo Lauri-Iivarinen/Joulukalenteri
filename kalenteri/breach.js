c73b8db3b628cec0e0f8038e9b750b53fb7d34df
Lauri Iivarinen <lauri@netti123.local>
2021-11-24 14:14:44 +0200

    muotoilu

---
+++ kalenteri/breach.js
@@ -0,0 +1,7 @@
+/*setInterval(function(){
+    var dice = Math.round(Math.random()*5+1);
+    document.getElementById("breach").innerHTML = "";
+    if (dice %2 ==0){
+        document.getElementById("breach").innerHTML = "Breach detected";
+    }
+}, 1000);*/
