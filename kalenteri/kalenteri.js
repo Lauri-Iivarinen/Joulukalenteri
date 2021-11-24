9de030d4383d4082d48820969eb7b221e5cef160
Lauri Iivarinen <lauri@netti123.local>
2021-11-24 15:52:13 +0200

    yhteinen funktio kuville

--- kalenteri/kalenteri.js
+++ kalenteri/kalenteri.js
@@ -3,12 +3,12 @@ var randomPaiva = [3,15,5,11,23,16,20,12,4,17,21,10,13,7,8,1,9,24,2,14,6,19,22,1
 
 console.log(randomPaiva.length)
 for (i=0; i<24; i++){
-    taulu += "<td><div class = 'tableInside' id='day"+randomPaiva[i]+"'><button class ='taulukko' onclick='funktio"+randomPaiva[i]+"("+randomPaiva[i]+");'>"+randomPaiva[i]+"</div></button></td>"
+    taulu += "<td><div class = 'tableInside' id='day"+randomPaiva[i]+"'><button class ='taulukko' onclick='funktio("+randomPaiva[i]+");'>"+randomPaiva[i]+"</div></button></td>"
 }
 
 document.getElementById("tableBase").innerHTML = taulu;
 //joka päivälle pitää tehdä funktio, testausta varten olemassa vain geneerinen "funktio"
-function funktio1(day){
+/*function funktio1(day){
     if(checkDay(day)){
         console.log("DAY 1")
         document.getElementById("day1").innerHTML = "<img src = 'kuvat/kuva1.png' height=150px width =150px'>"
@@ -18,9 +18,9 @@ function funktio1(day){
     }
 
     
-}
+}*/
 
-function funktio2(day){
+function funktio(day){
     if(checkDay(day)){
         console.log("DAY 2");
         document.getElementById("day"+day).innerHTML = "<img src = 'kuvat/kuva"+day+".png' height=150px width=150px'>"
