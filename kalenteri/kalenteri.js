bfaae01f00779a41b10cd4aad37201f7ae6c3c90
Lauri Iivarinen <lauri@netti123.local>
2021-11-26 20:29:54 +0200

    localstorage käytössä, aiemmin avatut kuvat aukeavat, kommentoitu pois

--- kalenteri/kalenteri.js
+++ kalenteri/kalenteri.js
@@ -1,28 +1,33 @@
 var taulu = "";
 var randomPaiva = [3,15,5,11,23,16,20,12,4,17,21,10,13,7,8,1,9,24,2,14,6,19,22,18];
+var date = new Date();
+var paivamaara = date.getDate()
+var month = date.getMonth();
+console.log(randomPaiva.length);
 
-console.log(randomPaiva.length)
+//avaa aiemmin samalla selaimella avatut taulut
+function openOld(){
+    //local.clear OTETTAVA pois ennen julkaisua
+    localStorage.clear();
+    /*KOMMENTOITU POIS TESTAUSTA VARTEN, OTETTAVA KÄYTTÖÖN VALMIISSA TUOTTEESSA
+    for(var i=1; i<=paivamaara;i++){
+        if(localStorage.getItem(i)==i){
+            funktio(i);
+        }
+    }*/
+}
 for (i=0; i<24; i++){
     taulu += "<td><div class = 'tableInside' id='day"+randomPaiva[i]+"'><button class ='taulukko' onclick='funktio("+randomPaiva[i]+");'>"+randomPaiva[i]+"</div></button></td>"
 }
 
 document.getElementById("tableBase").innerHTML = taulu;
 //joka päivälle pitää tehdä funktio, testausta varten olemassa vain geneerinen "funktio"
-/*function funktio1(day){
-    if(checkDay(day)){
-        console.log("DAY 1")
-        document.getElementById("day1").innerHTML = "<img src = 'kuvat/kuva1.png' height=150px width =150px'>"
-    }else{
-        console.log("wrong day");
-        //varmuuskopio
-    }
 
-    
-}*/
 
 function funktio(day){
     if(checkDay(day)){
         console.log("DAY 2");
+        localStorage.setItem(day,day);
         document.getElementById("day"+day).innerHTML = "<img src = 'kuvat/kuva"+day+".png' height=150px width=150px'>"
      }
     else{
@@ -34,9 +39,7 @@ function funktio(day){
 
 //palauttaa true jos klikattu luukku vastaa kyseistä päivämäärää
 function checkDay(day) {
-    var date = new Date();
-    var paivamaara = date.getDate()
-    var month = date.getMonth();
+    
     //month pitää vaihtaa joulukuussa "11"
     if (paivamaara >= day && month == 10){
         console.log("correct");
