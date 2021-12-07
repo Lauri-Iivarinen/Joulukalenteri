056fe2fac30a5e604d423b23ada0016ceba443e0
Kaminaah <voittokoivu9@gmail.com>
2021-12-07 19:07:22 +0200

    lisätty luukun avausanimaatio, kuvalle luotu oma container css + js taulukonluonti

--- kalenteri/kalenteri.js
+++ kalenteri/kalenteri.js
@@ -17,18 +17,28 @@ function openOld(){
     }*/
 }
 for (i=0; i<24; i++){
-    taulu += "<td><div class = 'tableInside' id='day"+randomPaiva[i]+"'><button class ='taulukko' onclick='funktio("+randomPaiva[i]+");'>"+randomPaiva[i]+"</div></button></td>"
+    taulu += "<td><div class = 'tableInside' id='day"+randomPaiva[i]+"'><div class='imageContainer' id='imageContainerId"+randomPaiva[i]+"'><button class ='taulukko'id='door"+randomPaiva[i]+"' onclick='funktio("+randomPaiva[i]+");'>"+randomPaiva[i]+"</div></button></div></td>"
 }
 
 document.getElementById("tableBase").innerHTML = taulu;
 //joka päivälle pitää tehdä funktio, testausta varten olemassa vain geneerinen "funktio"
 
 
+// ovenavaus animaation funktio
+// door1-24 = luukun avattava ovi --- backDoor1-24 elementti jossa kuva on
+function doorAnimation(day){
+    var element = document.querySelector(`#door${day}`);    //määrittä luukun jossa animaatio suoritetaan
+        element.classList.toggle("doorOpen");   //triggeröi ovenavaus animaation
+    var imageElement = document.getElementById(`imageContainerId${day}`);   //määrittää luukun johon kuva tulee
+        imageElement.style.backgroundImage = `url(../kalenteri/kuvat/kuva${day}.png)`;   //luo kuvan takaoveen
+    }
+
+
 function funktio(day){
     if(checkDay(day)){
         console.log("DAY 2");
         localStorage.setItem(day,day);
-        document.getElementById("day"+day).innerHTML = "<img src = 'kuvat/kuva"+day+".png' height=150px width=150px'>"
+        doorAnimation(day);
      }
     else{
         console.log("wrong day 2");
